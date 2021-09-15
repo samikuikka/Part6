18fe60e37d822fe57d4cad6f9d4e1790b377f421
Sami Kuikka <sami.kuikka@aalto.fi>
2021-09-15 15:36:19 +0300

    6.13

--- redux-anecdotes/src/App.js
+++ redux-anecdotes/src/App.js
@@ -1,11 +1,22 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import AnecdoteForm from './components/AnecdoteForm'
 import AnecdoteList from './components/AnecdoteList'
 import Notification from './components/Notification'
 import Filter from './components/Filter'
 
+import anecdoteService from './services/anecdotes'
+import { initialize } from './reducers/anecdoteReducer'
+import { useDispatch } from 'react-redux'
 
 const App = () => {
+  const dispatch = useDispatch()
+
+  useEffect( () => {
+    anecdoteService
+      .getAll().then(anecdotes => {
+        dispatch(initialize(anecdotes))
+      })
+  }, [dispatch])
 
   return (
     <div>
